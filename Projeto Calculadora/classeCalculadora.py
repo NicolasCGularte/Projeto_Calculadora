825b739b766b7bddd4de333bcd75c52e36ad05b0
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2023-02-13 11:47:43 -0300

    Update classeCalculadora.py

--- Projeto Calculadora/classeCalculadora.py
+++ Projeto Calculadora/classeCalculadora.py
@@ -1,42 +1,62 @@
 from Classes import Calculadora, Arquivo
 
 if __name__ == "__main__":
+    """
+    Inicialização da classe Calculadora e Arquivo.
+    """
     calculadora = Calculadora()
     arquivo = Arquivo()
 
     print("Bem-vindo à calculadora em Python")
     print("Exemplos de operações: '2 + 2', '5 - 3', '4 * 2', '6 / 2', '3 ** 2'")
 
-    while True:
-        entrada = input("Digite a operação ou 's' para sair: ")
-        if entrada == "s":
-            break
+while True:
+    """
+    Loop que recebe uma entrada do usuário e realiza as operações matemáticas.
+    """
+    entrada = input("Digite a operação ou 's' para sair: ")
+    if entrada == "s":
+        break
+    else:
+        try:
+            """
+            Tenta separar a entrada em 3 partes: número 1, operação e número 2.
+            """
+            num1, operacao, num2 = entrada.split(" ")
+            num1, num2 = int(num1), int(num2)
+        except ValueError:
+            """
+            Se a entrada não for válida, exibe mensagem de erro.
+            """
+            print("Erro: Insira uma operação válida.")
+            continue
         else:
-            try:
-                num1, operacao, num2 = entrada.split(" ")
-                num1, num2 = int(num1), int(num2)
-                # print(num1,num2)
-            except ValueError:
-                print("Erro: Insira uma operação válida.")
+            """
+            Verifica qual operação o usuário deseja realizar.
+            """
+            if operacao == "+":
+                resultado = calculadora.soma(num1, num2)
+                operacao = "soma"
+            elif operacao == "-":
+                resultado = calculadora.subtracao(num1, num2)
+                operacao = "subtração"
+            elif operacao == "*":
+                resultado = calculadora.multiplicacao(num1, num2)
+                operacao = "multiplicação"
+            elif operacao == "/":
+                resultado = calculadora.divisao(num1, num2)
+                operacao = "divisão"
+            elif operacao == "**":
+                resultado = calculadora.potencia(num1, num2)
+                operacao = "potência"
             else:
-                if operacao == "+":
-                    resultado = calculadora.soma(num1, num2)
-                    operacao = "soma"
-                elif operacao == "-":
-                    resultado = calculadora.subtracao(num1, num2)
-                    operacao = "subtração"
-                elif operacao == "*":
-                    resultado = calculadora.multiplicacao(num1, num2)
-                    operacao = "multiplicação"
-                elif operacao == "/":
-                    resultado = calculadora.divisao(num1, num2)
-                    operacao = "divisão"
-                elif operacao == "**":
-                    resultado = calculadora.potencia(num1, num2)
-                    operacao = "potência"
-                else:
-                    print("Erro: Operação inválida.")
-                    continue
-            finally:
-                print("Resultado:", resultado)
-                arquivo.salvar_resultado(resultado, operacao)
+                """
+                Se a operação não for válida, exibe mensagem de erro.
+                """
+                print("Erro: Operação inválida.")
+                continue
+            """
+            Exibe o resultado da operação.
+            """
+            print("Resultado:", resultado)
+            arquivo.salvar_resultado(resultado, operacao)

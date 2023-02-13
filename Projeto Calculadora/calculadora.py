fdff697b0daffd14136595b531c8cfa3a03b376c
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2023-02-13 11:49:38 -0300

    Update calculadora.py

--- Projeto Calculadora/calculadora.py
+++ Projeto Calculadora/calculadora.py
@@ -1,38 +1,62 @@
-def calculadora():
+from Classes import Calculadora, Arquivo
+
+if __name__ == "__main__":
+    """
+    Inicialização da classe Calculadora e Arquivo.
+    """
+    calculadora = Calculadora()
+    arquivo = Arquivo()
+
     print("Bem-vindo à calculadora em Python")
-    print("Selecione o número da operação desejada:")
-    print("1. Adição")
-    print("2. Subtração")
-    print("3. Multiplicação")
-    print("4. Divisão")
-    print("5. Sair")
+    print("Exemplos de operações: '2 + 2', '5 - 3', '4 * 2', '6 / 2', '3 ** 2'")
 
-    escolha = int(input("Digite escolha(1/2/3/4/5): "))
-
-    if escolha == 1:
-        num1 = float(input("Digite primeiro número: "))
-        num2 = float(input("Digite segundo número: "))
-        resultado = num1 + num2
-        print("Resultado da adição:", resultado)
-    elif escolha == 2:
-        num1 = float(input("Digite primeiro número: "))
-        num2 = float(input("Digite segundo número: "))
-        resultado = num1 - num2
-        print("Resultado da subtração:", resultado)
-    elif escolha == 3:
-        num1 = float(input("Digite primeiro número: "))
-        num2 = float(input("Digite segundo número: "))
-        resultado = num1 * num2
-        print("Resultado da multiplicação:", resultado)
-    elif escolha == 4:
-        num1 = float(input("Digite primeiro número: "))
-        num2 = float(input("Digite segundo número: "))
-        resultado = num1 / num2
-        print("Resultado da divisão:", resultado)
-    elif escolha == 5:
-        print("Saindo da calculadora...")
-        exit()
+while True:
+    """
+    Loop que recebe uma entrada do usuário e realiza as operações matemáticas.
+    """
+    entrada = input("Digite a operação ou 's' para sair: ")
+    if entrada == "s":
+        break
     else:
-        print("Opção inválida. Tente novamente.")
-
-calculadora()
+        try:
+            """
+            Tenta separar a entrada em 3 partes: número 1, operação e número 2.
+            """
+            num1, operacao, num2 = entrada.split(" ")
+            num1, num2 = int(num1), int(num2)
+        except ValueError:
+            """
+            Se a entrada não for válida, exibe mensagem de erro.
+            """
+            print("Erro: Insira uma operação válida.")
+            continue
+        else:
+            """
+            Verifica qual operação o usuário deseja realizar.
+            """
+            if operacao == "+":
+                resultado = calculadora.soma(num1, num2)
+                operacao = "soma"
+            elif operacao == "-":
+                resultado = calculadora.subtracao(num1, num2)
+                operacao = "subtração"
+            elif operacao == "*":
+                resultado = calculadora.multiplicacao(num1, num2)
+                operacao = "multiplicação"
+            elif operacao == "/":
+                resultado = calculadora.divisao(num1, num2)
+                operacao = "divisão"
+            elif operacao == "**":
+                resultado = calculadora.potencia(num1, num2)
+                operacao = "potência"
+            else:
+                """
+                Se a operação não for válida, exibe mensagem de erro.
+                """
+                print("Erro: Operação inválida.")
+                continue
+            """
+            Exibe o resultado da operação.
+            """
+            print("Resultado:", resultado)
+            arquivo.salvar_resultado(resultado, operacao)
